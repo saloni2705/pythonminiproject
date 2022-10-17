79c0f314171870e63d5a02d47d0a5f73f627e0e4
saloni2705 <92199915+saloni2705@users.noreply.github.com>
2022-10-17 20:30:04 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import googletrans
 import speech_recognition as sr
-
+import pyaudio
+import pyttsx3
 
 recognizer = sr.Recognizer()
 translator = googletrans.Translator()
+engine = pyttsx3.init()
 
-
+#for microphone
 try:
     with sr.Microphone() as source:
         print('Speak now')
@@ -13,11 +15,11 @@ try:
         text = recognizer.recognize_google(voice)
         print(text)
 
-except:
-    print('speak again')
+except :
+   print('speak again')
 
-translated = translator.translate(text , dest='en')
+#for translation
+langinput = input('Type the language you want to convert:')
+translated = translator.translate(text,langinput)
+
 print(translated.text)
-
-print(googletrans.LANGUAGES)
-
